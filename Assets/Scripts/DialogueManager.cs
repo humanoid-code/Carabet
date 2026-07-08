e11ac3a78e7717ab9afdc4343fda93a593d13b28
Love <strangenamgeagain@gmail.com>
2026-07-08 17:23:29 +0300

    love becomes main

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Ink.Runtime; // Обязательно!
+using Ink.Runtime;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,16 +10,14 @@ public class DialogueManager : MonoBehaviour
     private Story story;
 
     [Header("Связи с UI")]
-    [SerializeField] private TextMeshProUGUI dialogueText; // или просто Text, если юзаешь старый UI
+    [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private Transform choicesContainer;
     [SerializeField] private GameObject choiceButtonPrefab;
 
     void Start()
     {
-        Debug.Log("=== ТЕСТ: МЕНЕДЖЕР ЗАПУСТИЛСЯ ===");
         if (inkJsonAsset != null)
         {
-            Debug.Log("=== ТЕСТ: JSON НАЙДЕН ===");
             story = new Story(inkJsonAsset.text);
             story.ChoosePathString("start");
             RefreshView();
@@ -39,22 +37,18 @@ public class DialogueManager : MonoBehaviour
         }
         dialogueText.text = text;
 
-        // Если есть варианты выбора — создаем кнопки
-        Debug.Log("Количество выборов в Ink сейчас: " + story.currentChoices.Count);
+        // Если есть варианты выбора, то создаем кнопки
         if (story.currentChoices.Count > 0)
         {
             for (int i = 0; i < story.currentChoices.Count; i++)
             {
                 Choice choice = story.currentChoices[i];
                 GameObject button = Instantiate(choiceButtonPrefab, choicesContainer);
-                button.transform.localPosition = Vector3.zero; 
-                button.transform.localScale = Vector3.one;
-                Debug.Log("ФИЗИЧЕСКИ СОЗДАНА КНОПКА: " + choice.text + " внутри " + choicesContainer.name);
-                // Пишем текст на кнопке
-                button.GetComponentInChildren<TextMeshProUGUI>().text = choice.text;
+                //Instantiate(1, 2) делает копию префаба (1) в (2)
+                button.GetComponentInChildren<TextMeshProUGUI>().text = choice.text; // текст на кнопке
 
-                // Назначаем кнопке действие при клике
-                int index = i;
+                // Назначаем кнопке действие при клике (чоткая короткая запись через лямбду)
+                int index = i; //ОБЯЗАТЕЛЬНО
                 button.GetComponent<Button>().onClick.AddListener(() => {
                     story.ChooseChoiceIndex(index);
                     RefreshView();
